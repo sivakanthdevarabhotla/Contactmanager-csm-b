a0b746fc8cc43394d4e3267aeb5c2aba2239378f
sivakanthdevarabhotla <sivakanth.devarabhotla24@sasi.ac.in>
2026-07-13 15:01:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sivakanthdevarabhotla:<db_password>@cluster0.ik7cz4d.mongodb.net/sivakanth");
+        await mongoose.connect("mongodb+srv://sivakanthdevarabhotla:<siva11390>@cluster0.ik7cz4d.mongodb.net/sivakanth");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
