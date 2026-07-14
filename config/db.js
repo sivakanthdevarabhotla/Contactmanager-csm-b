97c0cd4fe733953f8c39432a2857bbd1030370a6
sivakanthdevarabhotla <sivakanth.devarabhotla24@sasi.ac.in>
2026-07-14 10:54:18 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
-
-
 const mongoose = require("mongoose");
 const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sivakanthdevarabhotla:<siva11390>@cluster0.ik7cz4d.mongodb.net/sivakanth");
+        await mongoose.connect("mongodb+srv://sivakanthdevarabhotla:siva11390@cluster0.ik7cz4d.mongodb.net/sivakanth");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
@@ -13,3 +11,6 @@ const connectDB = async () => {
     }
 };
 module.exports = connectDB;
+
+
+       
